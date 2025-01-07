bfa30a26f3609e5f2905f54e54bf4ccf304bef19
himanigupta <gupta.himani44@gmail.com>
2025-01-07 08:20:26 -0800

    Add dialog components and integrate AddQuiz

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,3 +1,4 @@
+import { AddQuiz } from "../dialogs/AddQuiz";
 import { Button } from "../ui/button";
 
 const Header = () => {
@@ -5,6 +6,7 @@ const Header = () => {
     <div className="border-b flex item-center justify-between p-2">
       <h1 className="text-3xl font-semibold">Quiz</h1>
       <Button>Add Quiz</Button>
+      <AddQuiz />
     </div>
   );
 };
