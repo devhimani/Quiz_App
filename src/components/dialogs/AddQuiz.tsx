6982367f9ab61d469dda5bcd02e2ab85cb52f986
himanigupta <gupta.himani44@gmail.com>
2025-01-07 09:23:03 -0800

    Enhance AddQuiz: Add text area, Duration, and Marks fields

--- src/components/dialogs/AddQuiz.tsx
+++ src/components/dialogs/AddQuiz.tsx
@@ -10,44 +10,45 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export function AddQuiz() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
+        <Button>Add Quiz</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
+          <DialogTitle>Add Quiz</DialogTitle>
+          <DialogDescription>Add a New Quiz!</DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
+            <Label htmlFor="description">Description</Label>
+            <Textarea id="description" className="col-span-3" />
+          </div>
+          <div className="flex items-center gap-4 w-full">
+            <div className="w-full">
+              <Label htmlFor="duration" className="text-right">
+                Duration (mins)
+              </Label>
+              <input id="duration" type="number" className="col-span-3" />
+            </div>
+            <div className="w-full">
+              <Label htmlFor="marks" className="text-right">
+                Marks
+              </Label>
+              <input id="marks" type="number" className="col-span-3" />
+            </div>
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit">Submit</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,11 +1,9 @@
 import { AddQuiz } from "../dialogs/AddQuiz";
-import { Button } from "../ui/button";
 
 const Header = () => {
   return (
     <div className="border-b flex item-center justify-between p-2">
       <h1 className="text-3xl font-semibold">Quiz</h1>
-      <Button>Add Quiz</Button>
       <AddQuiz />
     </div>
   );
