4c7b021ac6e51079623b871c7d79e0b71ee6795b
himanigupta <gupta.himani44@gmail.com>
2025-01-07 06:10:17 -0800

    Add basic routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <> Hello</>;
-};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import Home from "./pages/Home";
 
-export default App;
+export default function Home() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>Home</>}></Route>
+          <Route path="*" element={<>404</>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
