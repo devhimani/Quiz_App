fdbe291bf326e8ffb88e274f08f1c3e5c86f946a
himanigupta <gupta.himani44@gmail.com>
2025-01-09 00:02:34 -0800

    feat: implement AddQuiz form with state and validation

--- src/components/dialogs/AddQuiz.tsx
+++ src/components/dialogs/AddQuiz.tsx
@@ -11,8 +11,21 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 export function AddQuiz() {
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [marks, setMarks] = useState(0);
+
+  const handleSubmit = () => [console.log({ name, desc, duration, marks })];
+
+  // set data in context
+  // set data in firebase
+  // clear form
+  //close dialog
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -26,29 +39,57 @@ export function AddQuiz() {
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name">Name</Label>
-            <Input id="name" className="col-span-3" />
+            <Input
+              id="name"
+              className="col-span-3"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description">Description</Label>
-            <Textarea id="description" className="col-span-3" />
+            <Textarea
+              id="description"
+              className="col-span-3"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
           </div>
           <div className="flex items-center gap-4 w-full">
             <div className="w-full">
               <Label htmlFor="duration" className="text-right">
                 Duration (mins)
               </Label>
-              <input id="duration" type="number" className="col-span-3" />
+              <input
+                id="duration"
+                type="number"
+                className="col-span-3"
+                value={duration || ""}
+                onChange={(e) => setDuration(parseInt(e.target.value))}
+              />
             </div>
             <div className="w-full">
               <Label htmlFor="marks" className="text-right">
                 Marks
               </Label>
-              <input id="marks" type="number" className="col-span-3" />
+              <input
+                id="marks"
+                type="number"
+                className="col-span-3"
+                value={marks || ""}
+                onChange={(e) => setMarks(parseInt(e.target.value))}
+              />
             </div>
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Submit</Button>
+          <Button
+            type="submit"
+            onClick={handleSubmit}
+            disabled={!name || !desc || !duration || !marks}
+          >
+            Submit
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
