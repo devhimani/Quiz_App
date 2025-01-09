0a286ecc04564676ce088ad90691616a4c54fcb1
himanigupta <gupta.himani44@gmail.com>
2025-01-09 07:12:20 -0800

    feat(HomePage): render quizzes dynamically using map from context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,16 @@ const HomePage = () => {
   return (
     <div>
       <Header />
+      <div className="p-2 flex gap-2">
+        {quizzes.map((quiz) => (
+          <div key={quiz.id} className="border p-2 w-[150px]">
+            <h1 className="font-bold text-sm line-clamp-1">{quiz.name} </h1>
+            <p className="line-clamp-2">{quiz.desc}</p>
+            <p>{quiz.duration}</p>
+            <p>{quiz.marks}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
