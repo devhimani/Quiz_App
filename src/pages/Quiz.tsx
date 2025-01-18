b80f00928f3794c9246f91cf41f3b64b8ccd6cab
himanigupta <gupta.himani44@gmail.com>
2025-01-18 10:50:28 -0800

    Display quiz details and apply styling

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -24,7 +24,21 @@ const Quiz = () => {
 
   return (
     <div>
-      <AddQuestion />
+      <div className="border-b flex items-center justify-between p-2">
+        <h1 className="text-3xl font-semibold">Quiz</h1>
+        <AddQuestion />
+      </div>
+      <div className="p-2 space-y-2">
+        <div className="w-full flex items-center justify-center">
+          <h1 className="text-xl font-bold bg-gray-300 px-2 rounded">
+            {quiz.name}
+          </h1>
+        </div>
+        <div className="flex justify-between text-sm border border-r-0 border-l-0 border-t-0">
+          <p>Duration: {quiz.duration} mins</p>
+          <p>Marks: {quiz.marks}</p>
+        </div>
+      </div>
     </div>
   );
 };
