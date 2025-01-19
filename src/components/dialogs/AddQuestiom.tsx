412a76ff154ef62844360093b9a026ed16289c80
himanigupta <gupta.himani44@gmail.com>
2025-01-19 00:57:21 -0800

    AddQuestion component with form handling and state management

--- src/components/dialogs/AddQuestiom.tsx
+++ src/components/dialogs/AddQuestiom.tsx
@@ -9,14 +9,42 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "../ui/textarea";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { useState } from "react";
 
 export function AddQuestion() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [optionA, setOptionA] = useState("");
+  const [optionB, setOptionB] = useState("");
+  const [optionC, setOptionC] = useState("");
+  const [optionD, setOptionD] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const handleQuestions = () => {
+    const newQuestion = {
+      question,
+      optionA,
+      optionB,
+      optionC,
+      optionD,
+      answer,
+    };
+    console.log(newQuestion);
+
+    // send data to firebase
+
+    //store data to local context
+
+    // clear form
+
+    setIsOpen(false);
+  };
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button>Add Question</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent aria-describedby={undefined}>
         <DialogTitle>Add Question</DialogTitle>
         <div>
           <Label htmlFor="question">Question</Label>
@@ -24,6 +52,8 @@ export function AddQuestion() {
             id="message"
             placeholder="Please enter question"
             className="mt-1"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
           />
         </div>
         <div className="mt-[-5px]">
@@ -31,35 +61,59 @@ export function AddQuestion() {
           <div className="grid grid-cols-2 gap-2 mt-1">
             <div className="flex items-center gap-2">
               <Label htmlFor="a">(a)</Label>
-              <Input id="a" placeholder="option a" />
+              <Input
+                id="a"
+                placeholder="option a"
+                value={optionA}
+                onChange={(e) => setOptionA(e.target.value)}
+              />
             </div>
             <div className="flex items-center gap-2">
-              <Label htmlFor="a">(b)</Label>
-              <Input id="b" placeholder="option a" />
+              <Label htmlFor="b">(b)</Label>
+              <Input
+                id="b"
+                placeholder="option b"
+                value={optionB}
+                onChange={(e) => setOptionB(e.target.value)}
+              />
             </div>
             <div className="flex items-center gap-2">
-              <Label htmlFor="a">(c)</Label>
-              <Input id="c" placeholder="option a" />
+              <Label htmlFor="c">(c)</Label>
+              <Input
+                id="c"
+                placeholder="option c"
+                value={optionC}
+                onChange={(e) => setOptionC(e.target.value)}
+              />
             </div>
             <div className="flex items-center gap-2">
-              <Label htmlFor="a">(d)</Label>
-              <Input id="d" placeholder="option a" />
+              <Label htmlFor="d">(d)</Label>
+              <Input
+                id="d"
+                placeholder="option d"
+                value={optionD}
+                onChange={(e) => setOptionD(e.target.value)}
+              />
             </div>
           </div>
         </div>
         <div className="mt-[-5px]">
           <Label htmlFor="answer">Answer</Label>
-          <RadioGroup className="flex mt-1">
+          <RadioGroup
+            className="flex mt-1"
+            value={answer}
+            onValueChange={setAnswer}
+          >
             <div className="flex items-center space-x-2">
               <RadioGroupItem value="a" id="option-a" />
               <Label htmlFor="option-a">(a)</Label>
             </div>
             <div className="flex items-center space-x-2">
-              <RadioGroupItem value="b" id="option-a" />
+              <RadioGroupItem value="b" id="option-b" />
               <Label htmlFor="option-b">(b)</Label>
             </div>
             <div className="flex items-center space-x-2">
-              <RadioGroupItem value="c" id="option-a" />
+              <RadioGroupItem value="c" id="option-c" />
               <Label htmlFor="option-c">(c)</Label>
             </div>
             <div className="flex items-center space-x-2">
@@ -69,10 +123,10 @@ export function AddQuestion() {
           </RadioGroup>
         </div>
         <div className="flex gap-2 mt-2">
-          <div>
-            <Button variant="destructive">Destructive</Button>
-            <Button className="w-full">Add Question</Button>
-          </div>
+          <Button variant={"destructive"}>Clear All</Button>
+          <Button className="w-full" onClick={handleQuestions}>
+            Add Question
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
